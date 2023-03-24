af8362ddaf8504fc1cf06264aaafdb4fec531b7d
Vinod Kubsad <vinod.kubsad57@gmail.com>
2023-03-24 21:00:57 +0530

    login page css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { HttpClientModule } from '@angular/common/http';
+import { CoolSocialLoginButtonsModule } from '@angular-cool/social-login-buttons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { AuthService } from './shared/services/auth.service';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     SharedModule,
+    CoolSocialLoginButtonsModule
   ],
   providers: [
     [{ provide: JWT_OPTIONS, useValue: JWT_OPTIONS }, JwtHelperService],
